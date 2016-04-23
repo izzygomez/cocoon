9206ab8d6f4286674a9f5ee32c4442eb05b8709e
Ismael Gomez <izzygmit@gmail.com>
2016-04-23 16:48:03 -0400

    added home page, TODO routing to here

---
+++ routes/home.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var schemas = require('../models/schemas');
+
+var User = schemas.User;
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('home', { title: 'THE APP' });
+});
