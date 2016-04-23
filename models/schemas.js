0ae24288b1ec2f86825f5908e88d79bab3f4590c
Ismael Gomez <izzygmit@gmail.com>
2016-04-23 16:05:57 -0400

    added file name and contents in schema

--- models/schemas.js
+++ models/schemas.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
-  files: [{ type: String, required: true }]
+  files: [{ name: { type: String, required: true },
+            contents: { type: String, required: true } }]
 });
 
 module.exports = {
