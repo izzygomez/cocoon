b6fcde76bac916dcec73c51bbea09ce318e5fea2
Eric Chen <echentw@mit.edu>
2016-05-01 09:19:05 -0400

    upload files to server

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var multer = require('multer');
+var fs = require('fs');
 var router = express.Router();
+
+var uploads_dir = './uploads';
 
 var authenticate = function(req, res, next) {
   if (req.session.currentUser) {
@@ -11,11 +15,41 @@ var authenticate = function(req, res, next) {
 
 router.all('*', authenticate);
 
-/* GET home page. */
+/* GET upload page. */
 router.get('/', function(req, res, next) {
   var user = req.session.currentUser;
   var message = 'Logged in as: ' + user.username;
   res.render('upload', { user: user, message: message });
+});
+
+/* POST upload page. */
+router.post('/', function(req, res, next) {
+  var user = req.session.currentUser;
+  var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+      var dir = uploads_dir + '/' + user.username;
+      if (!fs.existsSync(uploads_dir)) {
+        fs.mkdirSync(uploads_dir);
+      }
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      callback(null, dir);
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    }
+  });
+
+  var upload = multer({ storage: storage }).single('ciphertextFile');
+  upload(req, res, function(err) {
+    console.log(req.file)
+    if (err) {
+      res.render('upload', { user: user, message: 'Error uploading file.' });
+    } else {
+      res.render('upload', { user: user, message: 'Upload successful!' });
+    }
+  });
 });
 
 
