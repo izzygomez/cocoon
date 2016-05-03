4e6ae4e5dd2c0ae06e5ffd7763a640a7c2315a02
Eric Chen <echentw@mit.edu>
2016-05-03 06:36:57 -0400

    bug fix and clean

--- public/js/file.js
+++ public/js/file.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 
+  K_D = 'This is a key123';
+  K_C = 'This is a key234';
+  K_D += K_D;
+  K_C += K_C;
+
+  IV_D = 'This is an IV456';
+  IV_C = 'This is an IV567';
+
+  function encrypt(key, iv, plaintext) {
+    var key_bit = sjcl.codec.utf8String.toBits(key);
+    var iv_bit = sjcl.codec.utf8String.toBits(iv);
+    var ptxt_bit = sjcl.codec.utf8String.toBits(plaintext);
+
+    var adata = [];
+    var prp = new sjcl.cipher.aes(key_bit);
+    var ciphertext = sjcl.mode.cbc.encrypt(prp, ptxt_bit, iv_bit, adata);
+
+    var actual_ctxt = sjcl.codec.base64.fromBits(ciphertext);
+    console.log(actual_ctxt);
+
+    return actual_ctxt;
+  };
+
+  function decrypt(key, iv, ciphertext) {
+    var key_bit = sjcl.codec.utf8String.toBits(key);
+    var iv_bit = sjcl.codec.utf8String.toBits(iv);
+
+    var ctxt_bit = sjcl.codec.base64.toBits(ciphertext);
+
+    var adata = [];
+    var prp = new sjcl.cipher.aes(key_bit);
+    var plaintext = sjcl.mode.cbc.decrypt(prp, ctxt_bit, iv_bit, adata);
+
+    var actual_plaintext = sjcl.codec.utf8String.fromBits(plaintext);
+    return actual_plaintext;
+  };
+
   function round1() {
     var queryString = $('#query').val();
     var filename = $('#filename').html();
@@ -45,25 +82,27 @@ $(document).ready(function() {
 
   function round2(encryptedIndex) {
     console.log('round 2 of communication protocol');
+
+    console.log(encryptedIndex);
+
     var filename = $('#filename').html();
 
     sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
 
-    K_C = 'This is a key234'
-    IV_C = 'This is an IV567'
-
     var queryString = $('#query').val();
     var length = queryString.length;
-    var adata = [];
-    var key_bit = sjcl.codec.utf8String.toBits(K_C);
-    var iv_bit = sjcl.codec.utf8String.toBits(IV_C);
-    var prp = new sjcl.cipher.aes(key_bit);
 
-    var ciphertext_bit = sjcl.codec.base64.toBits(encryptedIndex);
-    var decrypted_ciphertext = sjcl.mode.cbc.decrypt(prp, ciphertext_bit, iv_bit, adata);
-    var actual_decrypted_ctxt = sjcl.codec.base64.fromBits(decrypted_ciphertext);
+    var startIndex = decrypt(K_D, IV_D, encryptedIndex);
 
-    var startIndex = actual_decrypted_ctxt;
+    // var adata = [];
+    // var key_bit = sjcl.codec.utf8String.toBits(K_C);
+    // var iv_bit = sjcl.codec.utf8String.toBits(IV_C);
+    // var prp = new sjcl.cipher.aes(key_bit);
+    // var ciphertext_bit = sjcl.codec.base64.toBits(encryptedIndex);
+    // var decrypted_ciphertext = sjcl.mode.cbc.decrypt(prp, ciphertext_bit, iv_bit, adata);
+    // var actual_decrypted_ctxt = sjcl.codec.base64.fromBits(decrypted_ciphertext);
+
+    // var startIndex = actual_decrypted_ctxt;
 
     $.ajax({
       url: '/file/' + filename + '/query/2',
