a7d4dbf0622fdfb9e041fb848a2edf845e2b5906
Ismael Gomez <izzygmit@gmail.com>
2016-05-03 05:04:02 -0400

    added client side round 1 functionality

--- public/js/file.js
+++ public/js/file.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
   function round1() {
     var queryString = $('#query').val();
     var filename = $('#filename').html();
+    var T = [];
+    for (i=0; i<queryString.length; i++) {
+      var bitArray = sjcl.hash.sha256.hash(queryString.substring(0,i+1));
+      var formattedString = sjcl.codec.hex.fromBits(bitArray);
+      T = T.concat(formattedString);
+    }
+
+    console.log(T);
     console.log(queryString);
     console.log(filename);
     console.log('hi');
@@ -10,15 +18,20 @@ $(document).ready(function() {
       url: '/file/' + filename + '/query/1',
       type: 'POST',
       data: {
-        queryString: queryString
+        T: T
       },
       success: function(data) {
         var success = data.success;
         var message = data.message;
         if (success) {
-          console.log('success!');
-          console.log(message);
-          round2();
+          var found = data.found;
+          if (found) {
+            console.log('success!');
+            console.log(message);
+            round2(data.encryptedIndex);
+          } else {
+            $('#message').html(message);
+          }
         } else {
           $('#message').html(message);
         }
@@ -29,7 +42,7 @@ $(document).ready(function() {
     });
   };
 
-  function round2() {
+  function round2(encryptedIndex) {
     console.log('round 2 of communication protocol');
     $.ajax({
       url: '/file/' + filename + '/query/2',
