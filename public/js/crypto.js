e1b641066629fcda9aa4d6d6d734f888851af023
Eric Chen <echentw@mit.edu>
2016-05-11 07:55:20 -0400

    move js crypto functions into one file

--- public/js/crypto.js
+++ public/js/crypto.js
@@ -129,3 +129,71 @@ function secureUnpermute (index, totalIndices, key) {
   var roundThree = unpermute(roundTwo, totalIndices, key);
   return roundThree;
 }
+
+sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
+
+// CBC-MAC for authentication
+function CBC_MAC(key, iv, text) {
+  var keyBits = sjcl.codec.utf8String.toBits(key);
+  var ivBits = sjcl.codec.utf8String.toBits(iv);
+  var textBits = sjcl.codec.utf8String.toBits(text);
+
+  var adata = [];
+  var prp = new sjcl.cipher.aes(keyBits);
+  var ciphertext = sjcl.mode.cbc.encrypt(prp, textBits, ivBits, adata);
+
+  var hexCiphertext = sjcl.codec.hex.fromBits(ciphertext);
+
+  // get last 32 bits of hex ciphertext
+  var hexLength = hexCiphertext.length
+  var hexMAC = hexCiphertext.slice(hexLength - 32, hexLength);
+
+  var MACBits = sjcl.codec.hex.toBits(hexMAC);
+  var stringMAC = sjcl.codec.base64.fromBits(MACBits);
+
+  return stringMAC;
+}
+
+// pseudo-random function
+function F(key, plaintext) {
+  var key_hash_bit = sjcl.hash.sha256.hash(key);
+  var key_hash = sjcl.codec.hex.fromBits(key_hash_bit);
+
+  var ctxt_bit = sjcl.hash.sha256.hash(plaintext);
+  var ctxt = sjcl.codec.hex.fromBits(ctxt_bit);
+
+  var final_ctxt_bit = sjcl.hash.sha256.hash(ctxt + key_hash);
+  var final_ctxt = sjcl.codec.hex.fromBits(final_ctxt_bit);
+  return final_ctxt;
+};
+
+// AES encryption in CBC mode
+function encrypt(key, iv, plaintext) {
+  var key_bit = sjcl.codec.utf8String.toBits(key);
+  var iv_bit = sjcl.codec.utf8String.toBits(iv);
+  var ptxt_bit = sjcl.codec.utf8String.toBits(plaintext);
+
+  var adata = [];
+  var prp = new sjcl.cipher.aes(key_bit);
+  var ciphertext = sjcl.mode.cbc.encrypt(prp, ptxt_bit, iv_bit, adata);
+
+  var actual_ctxt = sjcl.codec.base64.fromBits(ciphertext);
+
+  return actual_ctxt;
+};
+
+// AES decryption in CBC mode
+function decrypt(key, iv, ciphertext) {
+  var key_bit = sjcl.codec.utf8String.toBits(key);
+  var iv_bit = sjcl.codec.utf8String.toBits(iv);
+
+  var ctxt_bit = sjcl.codec.base64.toBits(ciphertext);
+
+  var adata = [];
+  var prp = new sjcl.cipher.aes(key_bit);
+  var plaintext = sjcl.mode.cbc.decrypt(prp, ctxt_bit, iv_bit, adata);
+
+  var actual_plaintext = sjcl.codec.utf8String.fromBits(plaintext);
+  return actual_plaintext;
+};
+
